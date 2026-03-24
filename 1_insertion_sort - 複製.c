dff1998bf7d967541ec6b17e4d79888786b721e7
MK <mkchou.tw@gmail.com>
2026-03-25 01:03:51 +0800

    2

--- 1_insertion_sort - 複製.c
+++ 1_insertion_sort - 複製.c
@@ -13,32 +13,33 @@ int insertionSort(float *p_a, int arr_size)
         asm volatile(
 
 
-            "flw    fa0, 0(%[p_i])                \n\t" 
-    
-            "1:                                   \n\t" 
-            "bltz   %[j], 2f                      \n\t" 
+
+"flw    fa0, 0(%[p_i])          \n\t" // 1. key = *p_i (載入要插入的值)
             
-            "slli   t0, %[j], 2                   \n\t"
-            "add    t1, %[p_a], t0                \n\t" 
-            "flw    fa1, 0(t1)                    \n\t"
+            "1:                             \n\t" // while 迴圈開始標籤
+            "bltz   %[j], 2f                \n\t" // 2. if (j < 0) 跳到標籤 2 (結束迴圈)
+            
+            "slli   t0, %[j], 2             \n\t" // 3. t0 = j * 4 (計算位移量)
+            "add    t1, %[p_a], t0          \n\t" // 4. t1 = p_a + t0 (指向 A[j] 的記憶體位址)
+            "flw    fa1, 0(t1)              \n\t" // 5. fa1 = A[j] (載入 A[j])
 
-            "flt.s  t2, fa0, fa1                  \n\t" 
-            "beqz   t2, 2f                        \n\t" 
+            "flt.s  t2, fa0, fa1            \n\t" // 6. t2 = (key < A[j]) 
+            "beqz   t2, 2f                  \n\t" // 7. if (key >= A[j]) 跳到標籤 2 (結束迴圈)
 
-            "fsw    fa1, 4(t1)                    \n\t" 
-            "addi   %[j], %[j], -1                \n\t" 
-            "addi   %[shift_cnt], %[shift_cnt], 1 \n\t" 
-            "j      1b                            \n\t" 
+            "fsw    fa1, 4(t1)              \n\t" // 8. A[j+1] = A[j] (把較大的值往後挪)
+            "addi   %[j], %[j], -1          \n\t" // 9. j = j - 1
+            "addi   %[shift_cnt], %[shift_cnt], 1 \n\t" // 10. shift_cnt++
+            "j      1b                      \n\t" // 11. 跳回標籤 1 繼續比
 
-            "2:                                   \n\t" 
-            "addi   t0, %[j], 1                   \n\t" 
-            "slli   t0, t0, 2                     \n\t" 
-            "add    t1, %[p_a], t0                \n\t"
-            "fsw    fa0, 0(t1)                    \n\t" 
+            "2:                             \n\t" // 結束標籤
+            "addi   t0, %[j], 1             \n\t" // 12. t0 = j + 1
+            "slli   t0, t0, 2               \n\t" // 13. 計算 (j+1)*4
+            "add    t1, %[p_a], t0          \n\t" // 14. 找到 A[j+1] 的位址
+            "fsw    fa0, 0(t1)              \n\t" // 15. A[j+1] = key (把數字插進去)
 
-            : [j] "+r" (j), [shift_cnt] "+r" (shift_cnt) 
-            : [p_a] "r" (p_a), [p_i] "r" (p_i)           
-            : "t0", "t1", "t2", "fa0", "fa1", "memory"   
+            : [j] "+r" (j), [shift_cnt] "+r" (shift_cnt) // 輸出與輸入
+            : [p_a] "r" (p_a), [p_i] "r" (p_i)           // 輸入
+            : "t0", "t1", "t2", "fa0", "fa1", "memory"   // 會動到的暫存器
 
 
 
